4b1dbfcea5fa7fea0c231fceb1763f86760dd461
nirvair09 <chandradeep262003@gmail.com>
2025-11-07 14:04:11 +0530

    sevrer started

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const server=http.createServer();
 const wss=new WebSocketServer({server});
 
 
+wss.on("connection",(socket)=>{
+    console.log("client connected");
+})
+
+server.listen(3000,()=>{
+    console.log("WebSocket server running on port 3000");
+});
