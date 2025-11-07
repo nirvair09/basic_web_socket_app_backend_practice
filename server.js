1ae1fdd55ecd457406f882083602044ff5edc53b
nirvair09 <chandradeep262003@gmail.com>
2025-11-07 14:34:49 +0530

    server logic added

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ const wss=new WebSocketServer({server});
 
 wss.on("connection",(socket)=>{
     console.log("client connected");
-})
+
+    socket.on('message',(msg)=>{
+        console.log("received",msg.toString());
+
+        wss.clients.forEach(client=>{
+            if(client.readyState===client.OPEN){
+
+                client.send(msg.toString());
+                               
+            }
+        });
+    });
+
+    socket.on('close',()=>{
+        console.log("socket disconnected");
+    });
+});
+
 
 server.listen(3000,()=>{
     console.log("WebSocket server running on port 3000");
